90cf079c7d9231daf8df09058fac91dffc41b10e
Abbi Devins-Suresh <aesther2012@gmail.com>
2021-09-03 11:49:07 -0400

    cleaned up some print statements

--- TwoSum.py
+++ TwoSum.py
@@ -6,21 +6,12 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        count = 0
-        currentSum = 0
         result = []
         for i in range(0, len(nums)):
             for j in range(1, len(nums)):
-                print ("I")
-                print(nums[i])
-                print(i)
-                print("J")
-                print(nums[j])
-                print(j)
-                print("\n")
-                if (i == j and (nums[i] + nums[j] == target)):
+                if i == j and (nums[i] + nums[j] == target):
                     break
-                if (nums[i] + nums[j] == target):
+                if nums[i] + nums[j] == target:
                     result.append(i)
                     result.append(j)
                     return result
