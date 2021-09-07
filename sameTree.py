0c75ae6e7924752ba084a013708c10b98de851a5
Abbi Devins-Suresh <aesther2012@gmail.com>
2021-09-07 10:58:58 -0400

    same tree problem

---
+++ sameTree.py
@@ -0,0 +1,43 @@
+# Check if trees are the sam e
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+import typing
+
+
+class Solution:
+    def isSameTree(self, p: typing.Optional[TreeNode], q: typing.Optional[TreeNode]) -> bool:
+
+        if q and not p:
+            return False
+
+        if p and not q:
+            return False
+
+        if not p and not q:
+            return True
+
+        if p.val != q.val:
+            return False
+
+        if p.left and q.left:
+            result = self.isSameTree(p.left, q.left)
+            if not result:
+                return False
+        elif p.left or q.left:
+            return False
+
+        if p.right and q.right:
+            result = self.isSameTree(p.right, q.right)
+            if not result:
+                return False
+        elif p.right or q.right:
+            return False
+
+        return True
